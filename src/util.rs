fa8fce92da6b2700e3031588c84622948d0d878c
Atte Virtanen <aten.email@gmail.com>
2021-02-06 18:39:01 +0200

    Add some more tests

--- src/util.rs
+++ src/util.rs
@@ -98,65 +98,85 @@ pub fn format_duration_short(duration: &Duration) -> String {
     let mins = (duration.as_secs() / 60) % 60;
     let secs = duration.as_secs() % 60;
 
-    let mut out = String::new();
     if hours > 0 {
-        out.push_str(&format!("{:02}:", hours));
+        format!("{}:{:02}:{:02}", hours, mins, secs)
+    } else {
+        format!("{}:{:02}", mins, secs)
     }
-    out.push_str(&format!("{:02}:{:02}", mins, secs));
-    out
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-
     #[test]
-    fn test_separate_thousands_unsigned() {
-        assert_eq!(separate_thousands_unsigned(0), "0");
-        assert_eq!(separate_thousands_unsigned(1), "1");
-        assert_eq!(separate_thousands_unsigned(10), "10");
-        assert_eq!(separate_thousands_unsigned(100), "100");
-        assert_eq!(separate_thousands_unsigned(1_000), "1 000");
-        assert_eq!(separate_thousands_unsigned(10_000), "10 000");
-        assert_eq!(separate_thousands_unsigned(100_000), "100 000");
-        assert_eq!(separate_thousands_unsigned(1_000_000), "1 000 000");
-        assert_eq!(separate_thousands_unsigned(10_000_000), "10 000 000");
+    fn ellipsis_string() {
+        assert_eq!(super::ellipsis_string("testing", 5), "test\u{2026}");
+        assert_eq!(super::ellipsis_string("testing", 50), "testing");
     }
 
     #[test]
-    fn test_separate_thousands_signed() {
-        assert_eq!(separate_thousands_signed(0), "0");
-        assert_eq!(separate_thousands_signed(1), "1");
-        assert_eq!(separate_thousands_signed(10), "10");
-        assert_eq!(separate_thousands_signed(100), "100");
-        assert_eq!(separate_thousands_signed(1_000), "1 000");
-        assert_eq!(separate_thousands_signed(10_000), "10 000");
-        assert_eq!(separate_thousands_signed(100_000), "100 000");
-        assert_eq!(separate_thousands_signed(1_000_000), "1 000 000");
-        assert_eq!(separate_thousands_signed(10_000_000), "10 000 000");
-
-        assert_eq!(separate_thousands_signed(-0), "0");
-        assert_eq!(separate_thousands_signed(-1), "-1");
-        assert_eq!(separate_thousands_signed(-10), "-10");
-        assert_eq!(separate_thousands_signed(-100), "-100");
-        assert_eq!(separate_thousands_signed(-1_000), "-1 000");
-        assert_eq!(separate_thousands_signed(-10_000), "-10 000");
-        assert_eq!(separate_thousands_signed(-100_000), "-100 000");
-        assert_eq!(separate_thousands_signed(-1_000_000), "-1 000 000");
-        assert_eq!(separate_thousands_signed(-10_000_000), "-10 000 000");
+    fn separate_thousands_unsigned() {
+        assert_eq!(super::separate_thousands_unsigned(0), "0");
+        assert_eq!(super::separate_thousands_unsigned(1), "1");
+        assert_eq!(super::separate_thousands_unsigned(10), "10");
+        assert_eq!(super::separate_thousands_unsigned(100), "100");
+        assert_eq!(super::separate_thousands_unsigned(1_000), "1 000");
+        assert_eq!(super::separate_thousands_unsigned(10_000), "10 000");
+        assert_eq!(super::separate_thousands_unsigned(100_000), "100 000");
+        assert_eq!(super::separate_thousands_unsigned(1_000_000), "1 000 000");
+        assert_eq!(super::separate_thousands_unsigned(10_000_000), "10 000 000");
     }
 
     #[test]
-    fn test_separate_thousands_floating() {
-        assert_eq!(separate_thousands_floating(f64::NAN), "NaN");
-        assert_eq!(separate_thousands_floating(f64::INFINITY), "inf");
-        assert_eq!(separate_thousands_floating(f64::NEG_INFINITY), "-inf");
+    fn separate_thousands_signed() {
+        assert_eq!(super::separate_thousands_signed(0), "0");
+        assert_eq!(super::separate_thousands_signed(1), "1");
+        assert_eq!(super::separate_thousands_signed(10), "10");
+        assert_eq!(super::separate_thousands_signed(100), "100");
+        assert_eq!(super::separate_thousands_signed(1_000), "1 000");
+        assert_eq!(super::separate_thousands_signed(10_000), "10 000");
+        assert_eq!(super::separate_thousands_signed(100_000), "100 000");
+        assert_eq!(super::separate_thousands_signed(1_000_000), "1 000 000");
+        assert_eq!(super::separate_thousands_signed(10_000_000), "10 000 000");
 
-        assert_eq!(separate_thousands_floating(0.0), "0");
-        assert_eq!(separate_thousands_floating(-0.0), "0");
-        assert_eq!(separate_thousands_floating(1.0), "1");
-        assert_eq!(separate_thousands_floating(-1.0), "-1");
+        assert_eq!(super::separate_thousands_signed(-0), "0");
+        assert_eq!(super::separate_thousands_signed(-1), "-1");
+        assert_eq!(super::separate_thousands_signed(-10), "-10");
+        assert_eq!(super::separate_thousands_signed(-100), "-100");
+        assert_eq!(super::separate_thousands_signed(-1_000), "-1 000");
+        assert_eq!(super::separate_thousands_signed(-10_000), "-10 000");
+        assert_eq!(super::separate_thousands_signed(-100_000), "-100 000");
+        assert_eq!(super::separate_thousands_signed(-1_000_000), "-1 000 000");
+        assert_eq!(super::separate_thousands_signed(-10_000_000), "-10 000 000");
+    }
 
-        assert_eq!(separate_thousands_floating(-0.011_718_75), "-0.011 718 75");
+    #[test]
+    fn separate_thousands_floating() {
+        assert_eq!(super::separate_thousands_floating(f64::NAN), "NaN");
+        assert_eq!(super::separate_thousands_floating(f64::INFINITY), "inf");
+        assert_eq!(super::separate_thousands_floating(f64::NEG_INFINITY), "-inf");
+
+        assert_eq!(super::separate_thousands_floating(0.0), "0");
+        assert_eq!(super::separate_thousands_floating(-0.0), "0");
+        assert_eq!(super::separate_thousands_floating(1.0), "1");
+        assert_eq!(super::separate_thousands_floating(-1.0), "-1");
+
+        assert_eq!(super::separate_thousands_floating(-0.011_718_75), "-0.011 718 75");
+    }
+
+    #[test]
+    fn format_duration() {
+        use super::Duration;
+
+        let duration = Duration::from_secs(4 * 60 + 20);
+        assert_eq!(super::format_duration_long(&duration), "4 minutes, 20 seconds");
+        assert_eq!(super::format_duration_short(&duration), "4:20");
+
+        let duration = Duration::from_secs(7 * 60 * 60 + 4 * 60 + 20);
+        assert_eq!(super::format_duration_long(&duration), "7 hours, 4 minutes, 20 seconds");
+        assert_eq!(super::format_duration_short(&duration), "7:04:20");
+
+        let duration = Duration::from_secs(4 * 60);
+        assert_eq!(super::format_duration_long(&duration), "4 minutes");
+        assert_eq!(super::format_duration_short(&duration), "4:00");
     }
 }
