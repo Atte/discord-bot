4b0161068ab2b9a168552cbdd7ec8545928f9510
Atte Virtanen <aten.email@gmail.com>
2021-09-25 14:26:41 +0300

    Add MongoDB indexes

--- src/migrations.rs
+++ src/migrations.rs
@@ -3,9 +3,9 @@ use log::info;
 use mongodb::{
     bson::{doc, Document},
     error::Result as MongoResult,
-    options::UpdateOptions,
+    options::IndexOptions,
     results::UpdateResult,
-    Database,
+    Database, IndexModel,
 };
 
 async fn mongo_number_to_string(
@@ -21,7 +21,7 @@ async fn mongo_number_to_string(
                     field: { "$toString": format!("${}", field) }
                 }
             }],
-            UpdateOptions::default(),
+            None,
         )
         .await
 }
@@ -42,9 +42,37 @@ async fn mongo_number_array_to_string_array(
                     } }
                 }
             }],
-            UpdateOptions::default(),
+            None,
         )
         .await
+}
+
+async fn mongo_ensure_indexes(
+    db: &Database,
+    collection: &str,
+    indexes: Vec<(Document, bool)>,
+) -> Result<()> {
+    #[allow(unused_must_use)]
+    {
+        // ignore error: the collection might already exist
+        db.create_collection(collection, None).await;
+    }
+
+    let collection = db.collection::<Document>(collection);
+    collection.drop_indexes(None).await?; // YOLO
+    collection
+        .create_indexes(
+            indexes.into_iter().map(|(spec, unique)| {
+                IndexModel::builder()
+                    .keys(spec)
+                    .options(IndexOptions::builder().unique(unique).build())
+                    .build()
+            }),
+            None,
+        )
+        .await?;
+
+    Ok(())
 }
 
 pub async fn mongo(db: &Database) -> Result<()> {
@@ -53,5 +81,21 @@ pub async fn mongo(db: &Database) -> Result<()> {
     mongo_number_to_string(db, "sticky-roles", "user_id").await?;
     mongo_number_to_string(db, "sticky-roles", "guild_id").await?;
     mongo_number_array_to_string_array(db, "sticky-roles", "role_ids").await?;
+
+    info!("Building MongoDB indexes...");
+    mongo_ensure_indexes(db, "stats", vec![(doc! { "type": 1, "id": 1 }, true)]).await?;
+    mongo_ensure_indexes(
+        db,
+        "sticky-roles",
+        vec![(doc! { "guild_id": 1, "user_id": 1 }, true)],
+    )
+    .await?;
+    mongo_ensure_indexes(
+        db,
+        "gib-seen",
+        vec![(doc! { "image.id": 1 }, true), (doc! { "time": 1 }, false)],
+    )
+    .await?;
+
     Ok(())
 }
