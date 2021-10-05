3b36a18c1b5d025f5a6ad48c77dd031759453989
Atte Virtanen <aten.email@gmail.com>
2021-10-05 18:57:12 +0300

    optimize uikit css

--- webui/pre-render.tsx
+++ webui/pre-render.tsx
@@ -28,20 +28,20 @@ async function renderIndex() {
     await fs.promises.writeFile('./src/index.html', html, 'utf8');
 }
 
-async function renderIcons() {
-    const fnames = await promisify(glob)('./src/**/*.tsx', { nosort: true });
-    const icons = await Promise.all(
-        fnames.map(async (fname) => {
-            const source = await fs.promises.readFile(fname, 'utf8');
-            return Array.from(source.matchAll(/uk-icon="([^"]+)"/g), (match) => match[1]);
-        }),
-    );
-    const uniqueIcons = Array.from(new Set(icons.flat()));
+async function mapGlobFiles<T>(pattern: string, map: (source: string) => T[]): Promise<T[]> {
+    const fnames = await promisify(glob)(pattern, { nosort: true });
+    const resultSets = await Promise.all(fnames.map(async (fname) => map(await fs.promises.readFile(fname, 'utf8'))));
+    return resultSets.flat();
+}
 
+async function renderUikitScript() {
+    const icons = await mapGlobFiles('./src/**/*.tsx', (source) =>
+        Array.from(source.matchAll(/uk-icon="([^"]+)"/g), (match) => match[1]),
+    );
     const js = `
         import UIkit from 'uikit';
         UIkit.icon.add({
-            ${uniqueIcons
+            ${Array.from(new Set(icons))
                 .map((icon) => `'${icon}': require('bundle-text:uikit/src/images/icons/${icon}.svg'),`)
                 .join('\n')}
         });
@@ -49,7 +49,37 @@ async function renderIcons() {
     await fs.promises.writeFile('./src/uikit.ts', js, 'utf8');
 }
 
-Promise.all([renderIndex(), renderIcons()]).catch((err) => {
+async function renderUikitStyle() {
+    const components = await mapGlobFiles('./node_modules/uikit/src/less/components/_import.less', (source) =>
+        Array.from(source.matchAll(/@import "([^.]+)\.less";/g)).map((match) => match[1]),
+    );
+    const themes = await mapGlobFiles('./node_modules/uikit/src/less/theme/_import.less', (source) =>
+        Array.from(source.matchAll(/@import "([^.]+)\.less";/g)).map((match) => match[1]),
+    );
+    const both = Array.from(new Set(components.concat(themes)));
+
+    const used = new Set(
+        ['variables', 'mixin', 'base', 'utility', 'inverse'].concat(
+            await mapGlobFiles('./src/**/*.tsx', (source) => {
+                return both.filter((name) => new RegExp(`[^a-z]uk-${name}[^a-z]`).test(source));
+            }),
+        ),
+    );
+
+    // TODO: make a proper dependency solver
+    used.add('nav');
+
+    const usedComponents = components
+        .filter((name) => used.has(name))
+        .map((name) => `@import "uikit/src/less/components/${name}.less";`);
+    const usedThemes = themes
+        .filter((name) => used.has(name))
+        .map((name) => `@import "uikit/src/less/theme/${name}.less";`);
+
+    await fs.promises.writeFile('./src/uikit.less', usedComponents.concat(usedThemes).join('\n'), 'utf8');
+}
+
+Promise.all([renderIndex(), renderUikitScript(), renderUikitStyle()]).catch((err) => {
     console.error(err);
     process.exit(1);
 });
